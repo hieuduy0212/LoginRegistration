857f77ba250c257c0dfa8b995f18af12ca5c8a47
hieujava <duyhieu0212@gmail.com>
2022-10-03 10:11:12 +0700

    add project the first time

---
+++ src/main/java/com/loginregister/controller/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.loginregister.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = {"/register"})
+public class RegistrationController extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//get data from registration.jsp
+		
+	}
+}
